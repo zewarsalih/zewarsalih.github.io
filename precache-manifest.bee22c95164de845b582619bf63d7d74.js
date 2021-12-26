52b7ecb0da928ab70e01a616510976ee382e6c74
Zewar Salih <43804881+zewarsalih@users.noreply.github.com>
2021-12-26 07:15:04 +0100

    Add files via upload

---
+++ precache-manifest.bee22c95164de845b582619bf63d7d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f257248a29f69e1e31b4",
+    "url": "/static/css/main.01812d7b.chunk.css"
+  },
+  {
+    "revision": "f257248a29f69e1e31b4",
+    "url": "/static/js/main.f257248a.chunk.js"
+  },
+  {
+    "revision": "7ec2a7d8a6db755b0850",
+    "url": "/static/js/1.7ec2a7d8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4004de5e5909913003d67793bacba70d",
+    "url": "/index.html"
+  }
+];
